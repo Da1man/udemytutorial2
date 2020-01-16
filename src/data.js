af3e788bd6465485630f075619555a8ded27c5c3
Da1man <chaos4ever@mail.ru>
2020-01-16 21:26:57 +0300

    lesson 67 done

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const DATA =[
+  {
+    id: '1',
+    img: 'https://upload.wikimedia.org/wikipedia/en/0/09/Post-it_Brand_logo.png',
+    text: 'Awesome text for post 1',
+    date: new Date().toJSON(),
+    booked: true,
+  },
+  {
+    id: '2',
+    img: 'https://upload.wikimedia.org/wikipedia/en/0/09/Post-it_Brand_logo.png',
+    text: 'Awesome text for post 2',
+    date: new Date().toJSON(),
+    booked: true,
+  },
+  {
+    id: '3',
+    img: 'https://upload.wikimedia.org/wikipedia/en/0/09/Post-it_Brand_logo.png',
+    text: 'Awesome text for post 3',
+    date: new Date().toJSON(),
+    booked: false,
+  },
+  {
+    id: '4',
+    img: 'https://upload.wikimedia.org/wikipedia/en/0/09/Post-it_Brand_logo.png',
+    text: 'Awesome text for post 4',
+    date: new Date().toJSON(),
+    booked: false,
+  },
+  {
+    id: '5',
+    img: 'https://upload.wikimedia.org/wikipedia/en/0/09/Post-it_Brand_logo.png',
+    text: 'Awesome text for post 5',
+    date: new Date().toJSON(),
+    booked: false,
+  },
+]
